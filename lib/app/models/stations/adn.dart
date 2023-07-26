4be8ff91b37c1b07870727217d81d66827986523
ianpenalver <jabezpenalver2k20@gmail.com>
2023-07-26 12:44:38 +0800

    Added Ads police stations

--- lib/app/models/stations/adn.dart
+++ lib/app/models/stations/adn.dart
@@ -3,7 +3,7 @@ class Adn {
     {
       "name": "Agusan del Norte Police Provincial Office",
       "address": "Camp Col Rafael C Rodriguez, Libertad, Butuan City",
-      "avatar": "assets/images/station8.png",
+      "avatar": "assets/images/station-adn-0.png",
       "number": "+639985987274",
       "lat": "8.9448895",
       "long": "125.50496",
@@ -12,7 +12,7 @@ class Adn {
     {
       "name": "Cabadbaran City Police Station",
       "address": "P-3, Poblacion 9, Cabadbaran City, Agusan del Norte",
-      "avatar": "assets/images/station11.jpg",
+      "avatar": "assets/images/station-adn-1.jpg",
       "number": "+639985987278",
       "lat": "9.121962",
       "long": "125.545208",
@@ -21,7 +21,7 @@ class Adn {
     {
       "name": "Buenavista Municipal Police Station",
       "address": "P-4, Poblacion 3, Buenavista, Agusan del Norte",
-      "avatar": "assets/images/station12.jpg",
+      "avatar": "assets/images/station-adn-2.jpg",
       "number": "+639100904515",
       "lat": "8.97643266184839",
       "long": "125.404375752616",
@@ -30,7 +30,7 @@ class Adn {
     {
       "name": "Carmen Municipal Police Station",
       "address": "District Cabatuan, Brgy. Poblacion, Carmen, Agusan del Norte",
-      "avatar": "assets/images/station13.jpg",
+      "avatar": "assets/images/station-adn-3.jpg",
       "number": "+639985987279",
       "lat": "8.9902865",
       "long": "125.2960624",
@@ -39,7 +39,7 @@ class Adn {
     {
       "name": "Jabonga Municipal Police Station",
       "address": "P-1, Brgy. Poblacion, Jabonga, Agusan del Norte",
-      "avatar": "assets/images/station14.jpg",
+      "avatar": "assets/images/station-adn-4.jpg",
       "number": "+639985987230",
       "lat": "9.340793",
       "long": "125.51642",
@@ -48,7 +48,7 @@ class Adn {
     {
       "name": "Kitcharao Municipal Police Station",
       "address": "P-6, Brgy. Songkoy, Kitcharao, Agusan del Norte",
-      "avatar": "assets/images/station15.jpg",
+      "avatar": "assets/images/station-adn-5.jpg",
       "number": "+639999962071",
       "lat": "9.4527574",
       "long": "125.5714127",
@@ -57,7 +57,7 @@ class Adn {
     {
       "name": "Las Nieves Municipal Police Station",
       "address": "P-5, Poblacion, Las Nieves, Agusan del Norte",
-      "avatar": "assets/images/station16.jpg",
+      "avatar": "assets/images/station-adn-6.jpg",
       "number": "+639088108630",
       "lat": "8.7361",
       "long": "125.6007",
@@ -66,7 +66,7 @@ class Adn {
     {
       "name": "Magallanes Municipal Police Station",
       "address": "P-1, Brgy. Marcos, Magallanes, Agusan del Norte",
-      "avatar": "assets/images/station17.jpg",
+      "avatar": "assets/images/station-adn-7.jpg",
       "number": "+639985987283",
       "lat": "9.0268",
       "long": "125.5193",
@@ -75,7 +75,7 @@ class Adn {
     {
       "name": "Nasipit Municipal Police Station",
       "address": "Poblacion 4, Nasipit, Agusan del Norte",
-      "avatar": "assets/images/station18.jpg",
+      "avatar": "assets/images/station-adn-8.jpg",
       "number": "+639985590530",
       "lat": "8.987683",
       "long": "125.341242",
@@ -84,7 +84,7 @@ class Adn {
     {
       "name": "Remedios Trinidad Romualdez Municipal Police Station",
       "address": "P-6, Poblacion 1, RTR, Agusan del Norte",
-      "avatar": "assets/images/station19.jpg",
+      "avatar": "assets/images/station-adn-9.jpg",
       "number": "+639512398906",
       "lat": "9.0536",
       "long": "125.5853",
@@ -94,7 +94,7 @@ class Adn {
       "name": "Santiago Municipal Police Station",
       "address":
           "Sweethome Village, P-10, Poblacion 1, Santiago, Agusan del Norte",
-      "avatar": "assets/images/station20.jpg",
+      "avatar": "assets/images/station-adn-10.jpg",
       "number": "+639985604669",
       "lat": "9.262462",
       "long": "125.553555",
@@ -103,7 +103,7 @@ class Adn {
     {
       "name": "Tubay Municipal Police Station",
       "address": "Lungsod Daan, Poblacion 2, Tubay, Agusan del Norte",
-      "avatar": "assets/images/station21.jpg",
+      "avatar": "assets/images/station-adn-11.jpg",
       "number": "+639385401158",
       "lat": "9.1694",
       "long": "125.5294",
--- lib/app/models/stations/butuan.dart
+++ lib/app/models/stations/butuan.dart
@@ -3,7 +3,7 @@ class Butuan {
     {
       "name": "Butuan City Police Office - PIO",
       "address": "Malvar Circle, Brgy. Holy Redeemer, Butuan City",
-      "avatar": "assets/images/station1.png",
+      "avatar": "assets/images/station-bc-0.png",
       "number": "+639985987292",
       "lat": "8.954789",
       "long": "125.531242",
@@ -12,7 +12,7 @@ class Butuan {
     {
       "name": "Butuan City Police Station 1",
       "address": "JC Aquino Ave., AD Curato St. Butuan City",
-      "avatar": "assets/images/station3.png",
+      "avatar": "assets/images/station-bc-1.png",
       "number": "+639985987293",
       "lat": "8.9470874",
       "long": "125.5431909",
@@ -21,7 +21,7 @@ class Butuan {
     {
       "name": "Butuan City Police Station 2",
       "address": "J. Satorre St., Butuan City",
-      "avatar": "assets/images/station4.png",
+      "avatar": "assets/images/station-bc-2.png",
       "number": "+639985987295",
       "lat": "8.958457",
       "long": "125.534016",
@@ -30,7 +30,7 @@ class Butuan {
     {
       "name": "Butuan City Police Station 3",
       "address": "Bayanihan, Butuan City",
-      "avatar": "assets/images/station5.png",
+      "avatar": "assets/images/station-bc-3.png",
       "number": "+639985987297",
       "lat": "8.9404688",
       "long": "125.5245282",
@@ -39,7 +39,7 @@ class Butuan {
     {
       "name": "Butuan City Police Station 4",
       "address": "P-3B, Ampayon, Butuan City",
-      "avatar": "assets/images/station6.png",
+      "avatar": "assets/images/station-bc-4.png",
       "number": "+639985987299",
       "lat": "8.957057",
       "long": "125.605488",
@@ -48,7 +48,7 @@ class Butuan {
     {
       "name": "Butuan City Police Station 5",
       "address": "San Mateo, Butuan City",
-      "avatar": "assets/images/station7.png",
+      "avatar": "assets/images/station-bc-5.png",
       "number": "+639985987301",
       "lat": "8.784832",
       "long": "125.563433",
