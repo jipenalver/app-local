e8befa2d6c6b61e77cc88adefd5062fd671e346a
ianpenalver <jabezpenalver2k20@gmail.com>
2023-07-30 01:00:12 +0800

    Added Tile Caching

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,20 @@ import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterMapTileCaching.initialise();
+  await FMTC.instance('mapStore').manage.createAsync();
   runApp(MyApp());
 }
+
+// void main() {
+//   runApp(MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -184,6 +192,7 @@ class MapsPage extends StatelessWidget {
             TileLayer(
               urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
               userAgentPackageName: 'com.example.juana_help',
+              tileProvider: FMTC.instance('mapStore').getTileProvider(),
             ),
             CurrentLocationLayer(
               followOnLocationUpdate: FollowOnLocationUpdate.always,
